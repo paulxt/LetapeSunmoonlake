50a8ca282b87034d26d35807eb31262ef19db071
paulxt <104313389+paulxt@users.noreply.github.com>
2024-12-24 00:43:53 +0800

    Adjust the partner-section

--- src/components/HomeUI/PartnerSection.js
+++ src/components/HomeUI/PartnerSection.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 //image
-import PartnerSectionImg1 from '../../assets/img/partner-section-img1.png'; //台灣
-import PartnerSectionImg2 from '../../assets/img/partner-section-img2.png'; //交通部
+import PartnerSectionImg1 from '../../assets/img/partner-section-img1.png'; //環境部
+import PartnerSectionImg2 from '../../assets/img/partner-section-img2.jpg'; //交通部
 import PartnerSectionImg3 from '../../assets/img/logo-nu.png'; //紐崔萊
 import PartnerSectionImg4 from '../../assets/img/partner-section-img4.png'; //KPLUS
 import PartnerSectionImg5 from '../../assets/img/partner-section-img5.png';
@@ -13,10 +13,19 @@ import PartnerSectionImg8 from '../../assets/img/partner-section-img8.png'; //�
 import PartnerSectionImg9 from '../../assets/img/partner-section-img9.png'; //日管處
 import PartnerSectionImg10 from '../../assets/img/partner-section-img10.png'; //Titan
 import PartnerSectionImg11 from '../../assets/img/partner-section-img11.png'; //Bone
-import PartnerSectionImg12 from '../../assets/img/partner-section-img12.png'; //南投縣
+import PartnerSectionImg12 from '../../assets/img/partner-section-img12.png'; //南投縣政府
 import PartnerSectionImg13 from '../../assets/img/partner-section-img13.png'; //推廣協會
 import PartnerSectionImg14 from '../../assets/img/partner-section-img14.png'; //Oakley
-import PartnerSectionImg15 from '../../assets/img/partner-section-img15.jpg'; //Oakley
+import PartnerSectionImg15 from '../../assets/img/partner-section-img15.jpg'; //Deya
+import PartnerSectionImg16 from '../../assets/img/partner-section-img16.png'; //黑妞
+import PartnerSectionImg17 from '../../assets/img/partner-section-img17.png'; //Castrol
+import PartnerSectionImg18 from '../../assets/img/partner-section-img18.png'; //Eva Air長榮
+import PartnerSectionImg19 from '../../assets/img/partner-section-img19.png'; //FACTOR
+import PartnerSectionImg20 from '../../assets/img/partner-section-img20.png'; //雷文虎克
+import PartnerSectionImg21 from '../../assets/img/partner-section-img21.png'; //SHIMANA
+import PartnerSectionImg22 from '../../assets/img/partner-section-img22.png'; //Volvo
+import PartnerSectionImg23 from '../../assets/img/partner-section-img23.png'; //公勝保經
+import PartnerSectionImg24 from '../../assets/img/partner-section-img24.jpg'; //Viva萬歲牌
 
 
 
@@ -31,29 +40,29 @@ const PartnerSection = ({language}) => {
                 </Row>
                 <Row className='align-items-center text-center my-4'>
                     <h2>指導單位 | Advisory Organization</h2>
-                    <Col size={12} xs={6} md={3} sm={6} >
+                    {/* 環境部 */}
+                    <Col size={12} xs={6} md={6} sm={6} >
                         <a href="https://www.taiwan.net.tw/w1.aspx" target='_blank' rel='noreferrer noopener'>
                             <div className="img-bx">
                                 <img className='w-100' src={PartnerSectionImg1} alt="" />
                             </div>
                         </a>
-                        
                     </Col>
-                    <Col  size={12} xs={6} md={3} sm={6}>
+                    <Col  size={12} xs={6} md={6} sm={6}>
                         <a href="https://www.taiwan.net.tw/w1.aspx" target='_blank' rel='noreferrer noopener'>
                             <div className="img-bx">
                                 <img className='w-100' src={PartnerSectionImg2} alt="" />
                             </div>
                         </a>
                     </Col>
-                    <Col size={12} xs={6} md={3} sm={6} >
+                    <Col size={12} xs={6} md={6} sm={6} >
                         <a href="https://www.nantou.gov.tw/big5/index.php" target='_blank' rel='noreferrer noopener'>
                             <div className="img-bx">
                                 <img src={PartnerSectionImg12} alt="" />
                             </div>
                         </a>
                     </Col>
-                    <Col size={12} xs={6} md={3} sm={6} >
+                    <Col size={12} xs={6} md={6} sm={6} >
                         <a href="https://www.aso.fr/en/" target='_blank' rel='noreferrer noopener'>
                             <div className="img-bx">
                                 <img className='w-100' src={PartnerSectionImg5} alt="" />
@@ -62,7 +71,7 @@ const PartnerSection = ({language}) => {
                     </Col>
                 </Row>
                 <Row className='align-items-center text-center my-4'>
-                    <h2>主辦單位 | Organier</h2>
+                    <h2>主辦單位 | Organizer</h2>
                     <Col xs={6}>
                         <div className="img-bx">
                             <img src={PartnerSectionImg13} alt="" />
@@ -114,38 +123,103 @@ const PartnerSection = ({language}) => {
                             </div>
                         </a>
                     </Col>
-                    <Col size={12} xs={6} md={3} sm={6} >
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
                         <a href="https://www.titan-tech.com.tw/" target='_blank' rel='noreferrer noopener'>
                             <div className="img-bx">
                                 <img className='w-75' src={PartnerSectionImg10} alt="" />
                             </div>
                         </a>
                     </Col>
-                    <Col size={12} xs={6} md={3} sm={6} >
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
                         <a href="https://www.boneshop.com/tw/" target='_blank' rel='noreferrer noopener'>
                             <div className="img-bx">
                                 <img className='w-75' src={PartnerSectionImg11} alt="" />
                             </div>
                         </a>
                     </Col>
-                    <Col size={12} xs={6} md={3} sm={6} >
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
                         <a href="https://tw.oakley.com/zh" target='_blank' rel='noreferrer noopener'>
                             <div className="img-bx">
                                 <img className='w-75' src={PartnerSectionImg14} alt="" />
                             </div>
                         </a>
                     </Col>
-                    <Col size={12} xs={6} md={3} sm={6} >
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
                         <a href="https://www.mommyhappy.com/" target='_blank' rel='noreferrer noopener'>
                             <div className="img-bx">
                                 <img className='w-75' src={PartnerSectionImg6} alt="" />
                             </div>
                         </a>
                     </Col>
-                    <Col size={12} xs={6} md={3} sm={6} >
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
                         <a href="https://www.deya.tw" target='_blank' rel='noreferrer noopener'>
                             <div className="img-bx">
                                 <img className='w-75' src={PartnerSectionImg15} alt="" />
+                            </div>
+                        </a>
+                    </Col>
+
+                    {/* 新增贊助商 16-24 */}
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
+                        <a href="https://www.deya.tw" target='_blank' rel='noreferrer noopener'>
+                            <div className="img-bx">
+                                <img className='w-75' src={PartnerSectionImg16} alt="" />
+                            </div>
+                        </a>
+                    </Col>
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
+                        <a href="https://www.castrol.com/" target='_blank' rel='noreferrer noopener'>
+                            <div className="img-bx">
+                                <img className='w-75' src={PartnerSectionImg17} alt="" />
+                            </div>
+                        </a>
+                    </Col>
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
+                        <a href="https://www.evaair.com/zh-tw/index.html" target='_blank' rel='noreferrer noopener'>
+                            <div className="img-bx">
+                                <img className='w-75' src={PartnerSectionImg18} alt="" />
+                            </div>
+                        </a>
+                    </Col>
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
+                        <a href="https://factorbikes.com/" target='_blank' rel='noreferrer noopener'>
+                            <div className="img-bx">
+                                <img className='w-75' src={PartnerSectionImg19} alt="" />
+                            </div>
+                        </a>
+                    </Col>
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
+                        <a href="https://lwhklab.com.tw/" target='_blank' rel='noreferrer noopener'>
+                            <div className="img-bx">
+                                <img className='w-75' src={PartnerSectionImg20} alt="" />
+                            </div>
+                        </a>
+                    </Col>
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
+                        <a href="https://www.shimano.com/en/" target='_blank' rel='noreferrer noopener'>
+                            <div className="img-bx">
+                                <img className='w-75' src={PartnerSectionImg21} alt="" />
+                            </div>
+                        </a>
+                    </Col>
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
+                        <a href="https://www.volvo.com/en/" target='_blank' rel='noreferrer noopener'>
+                            <div className="img-bx">
+                                <img className='w-75' src={PartnerSectionImg22} alt="" />
+                            </div>
+                        </a>
+                    </Col>
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
+                        <a href="https://www.goldennet.com.tw/tw" target='_blank' rel='noreferrer noopener'>
+                            <div className="img-bx">
+                                <img className='w-75' src={PartnerSectionImg23} alt="" />
+                            </div>
+                        </a>
+                    </Col>
+                    <Col size={12} xs={6} md={3} sm={6} className='py-4'>
+                        <a href="https://viva.lianhwa.com.tw/" target='_blank' rel='noreferrer noopener'>
+                            <div className="img-bx">
+                                <img className='w-75' src={PartnerSectionImg24} alt="" />
                             </div>
                         </a>
                     </Col>
